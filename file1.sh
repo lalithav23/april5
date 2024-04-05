d8826f910363acef30bfa6efc64d0f22d466babb
lalithav23 <164750392+lalithav23@users.noreply.github.com>
2024-04-05 15:05:27 +0530

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 sum=`expr 10 + 20`
+sub=`expr 10 - 20`
+prod=`expr 10 \* 20`
+div=`expr 10 \ 20`
 echo "The sum is $sum"
+echo "The sum is $sub"
+echo "The sum is $prod"
+echo "The sum is $div"
