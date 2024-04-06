0774c9665344e1f6729a5c4be7b8dd499bb1e564
lalithav23 <164750392+lalithav23@users.noreply.github.com>
2024-04-06 15:46:04 +0530

    Update file5.sh

--- file5.sh
+++ file5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-disk_usage=`df -h . | awk -F " " NR==2 {print $5} | sed s/%//Ig`
+disk_usage=`df -h . | awk -F " " 'NR==2 {print $5}' | sed s/%//Ig`
 if [ $disk_usage -gt 30 ]
 then
-echo " Disk usage reaches threshhold " | mail -s "Disk Usage Alert" lalithav233@gmail.com
+echo " Disk usage reaches threshhold "
 fi
